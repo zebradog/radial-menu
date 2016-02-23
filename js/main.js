6b1ad4714e94911f8849e742d472212fe90fe752
Matt Cook <matt@zebradog.com>
2016-02-23 14:30:06 -0600

    update from justcoded

--- js/main.js
+++ js/main.js
@@ -1,30 +1,43 @@
 var mainMenu = new radialMenu({
   x: 400,
-  y: 400,
-  spacing: 10
+  y: 300,
+  "start-radius":100
 });
 
+// FIRST LEVEL ITEMS
+// SIZE MORE THEN 1 - IGNORING THIS
 var child1 = mainMenu.add("CHILD", {
-  "stroke-color": "#000000",
+  "stroke-color": "#6cff00",
   "stroke-opacity": 0.5,
   "fill": "#FFFFFF",
-  "fill-opacity": 1 
+  "fill-opacity": 1,
+  "size": 0.2,
+  "data": {"a": 123, "b": 234},
+  "onclick": function () {
+    console.log(this.data);
+  }
 });
-var child2 = mainMenu.add("CHILD LOREM IPSUM");
-var child3 = mainMenu.add("CHILD");
-var child4 = mainMenu.add("Level1");
-var child5 = mainMenu.add("Level1");
-var child6 = mainMenu.add("Level1");
+var child2 = mainMenu.add("CHILD", {
+  "size": 0.3
+});
+var child3 = mainMenu.add("CHILD", {
+  "size": 0.6
+});
 
-var child11 = child1.add("CHILD");
-var child12 = child1.add("Level21");
-var child13 = child1.add("Level21");
+// SECOND LEVEL ITEMS
+// SIZE 1
+var child11 = child1.add("CHILD", {
+  size: 0.4
+});
+var child12 = child1.add("Level21", {
+  size: 0.6
+});
 
 var child21 = child2.add("Level22", {
-  "stroke-color": "#000000",
+  "stroke-color": "#6cff00",
   "stroke-opacity": 0.5,
   "fill": "#FFFFFF",
-  "fill-opacity": 1 
+  "fill-opacity": 1
 });
 var child22 = child2.add("Level22");
 var child23 = child2.add("Level22");
@@ -41,35 +54,104 @@ var child112 = child11.add("Level31");
 var child113 = child11.add("Level31");
 var child114 = child11.add("Level31");
 
+var child211 = child21.add("CHILD");
+var child212 = child21.add("Level31");
+var child213 = child21.add("Level31");
+var child214 = child21.add("Level31");
+var child215 = child21.add("Level31");
+var child216 = child21.add("Level31");
+
 var child1111 = child111.add("Level41 With Custom Event", {
-  "stroke-color": "#000000",
+  "stroke-color": "#6cff00",
   "stroke-opacity": 0.5,
   "fill": "#FFFFFF",
   "fill-opacity": 1,
+  "data": "#some_id",
   "onclick": function () {
-    alert("custom event");
+    alert("this.data = " + this.data);
   }
 });
-var child1112 = child111.add("Level41");
+var child1112 = child111.add("Level41", {
+  "font-size": "20px",
+  "font-family": 'Tahoma',
+  "font-color": '#FFFFFF',
+  "active-font-color": '#6cff00',
+  "active-stroke-color": '#ff0000',
+  "active-stroke-opacity": 1.0,
+  "active-fill": '#000000',
+  "active-fill-opacity": 1.0,
+  "stroke-color": '#000000',
+  "stroke-opacity": 1.0,
+  "fill": '#0012ff',
+  "fill-opacity": 0.5
+});
 var child1113 = child111.add("Level41");
-var child1114 = child111.add("Level41");
 
 var opened = false;
 
 window.addEventListener("click", function (e) {
   e.preventDefault();
+  e.stopPropagation();
   var x = e.pageX,
     y = e.pageY;
 
   if (!opened) {
     mainMenu.update({
-      x: x,
-      y: y
+      x: e.pageX,
+      y: e.pageY
     });
 
     mainMenu.open();
     opened = true;
   }
+});
+var link = document.getElementById("link");
+link.addEventListener("click", function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  child21.open();
+});
 
-    
+var link_close = document.getElementById("close-link");
+link_close.addEventListener("click", function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  var element = mainMenu.get();
+
+  setTimeout(function () {
+    element.transform("0");
+  }, 1000);
+
+  mainMenu.close();
+  opened = false;
+});
+
+var link_close_item = document.getElementById("close");
+link_close_item.addEventListener("click", function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  child2.close();
+});
+
+var link_children = document.getElementById("children");
+link_children.addEventListener("click", function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  console.log(mainMenu.children());
+});
+
+var item_children = document.getElementById("item-children");
+item_children.addEventListener("click", function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  console.log(child2.children());
+});
+
+var animate_link = document.getElementById("animate");
+animate_link.addEventListener("click", function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  var element = mainMenu.get();
+
+  element.animate({transform: 'translate(500)'}, 1000, mina.bounce);
 });
--- js/radialMenu.js
+++ js/radialMenu.js
@@ -19,36 +19,84 @@
     return c;
   };
 
+  /** checking if circles have size parameter */
+  function checkSize(arr) {
+    var counter = 0,
+      flag = false;
+
+    for (var i=0; i<arr.length; i++) {
+      if (arr[i].options.size) {
+        counter += arr[i].options.size;
+      }
+    }
+
+    if (counter > 1) {
+      return false;
+    } else {
+      if (counter === 0) {
+        return false;
+      } else {
+        return counter.toFixed(1);
+      }
+    }
+  }
+
+  /** building custom steps array */
+  function buildStepsArr(arr, number) {
+    var steps_arr = [],
+      no_size_qty = 0;
+
+    if (number === 1) {
+      return returnStepsArr(arr);
+    } else {
+      arr.forEach(function (el, index) {
+        if (!el.options.size) {
+          no_size_qty++;
+        }
+      });
+      for (var i=0; i<arr.length; i++) {
+        if (!arr[i].options.size) {
+          arr[i].options.size = (1-number)/no_size_qty;
+        }
+      };
+      return returnStepsArr(arr);
+    }
+
+    function returnStepsArr(new_arr) {
+      for (var i=0; i<new_arr.length; i++) {
+        steps_arr.push(360*new_arr[i].options.size);
+      }
+      return steps_arr;
+    }
+  };
+
   /** circlePoints: step, big radius, small radius, center point x/y, spacing between items */
-  function circlePoints(step, r1, r2, cx, cy, spacing) {
+  function circlePoints(children, step, r1, r2, cx, cy, spacing) {
     var points = [],
       x1Before,y1Before,x2Before,y2Before,x1After,y1After,x2After,y2After,
       flag = 0,
       circleLengthR1 = 2*Math.PI*r1,
       circleLengthR2 = 2*Math.PI*r2,
-      spacingR1 = (spacing/2)*360/circleLengthR1,
-      spacingR2 = (spacing/2)*360/circleLengthR2;
+      spacingR1Left = (spacing/2)*360/circleLengthR1,
+      spacingR1Right = (spacing/2)*360/circleLengthR1,
+      spacingR2Left = (spacing/2)*360/circleLengthR2,
+      spacingR2Right = (spacing/2)*360/circleLengthR2,
+      custom = checkSize(children),
+      steps = [];
 
-    for (var i=0; i<=360; i+=step) {
-      x1Before = Math.round(cx + r1 * Math.cos((i-spacingR1) * (Math.PI/180))),
-      y1Before = Math.round(cy + r1 * Math.sin((i-spacingR1) * (Math.PI/180))),
-      x2Before = Math.round(cx + r2 * Math.cos((i-spacingR2) * (Math.PI/180))),
-      y2Before = Math.round(cy + r2 * Math.sin((i-spacingR2) * (Math.PI/180))),
-      x1After = Math.round(cx + r1 * Math.cos((i+spacingR1) * (Math.PI/180))),
-      y1After = Math.round(cy + r1 * Math.sin((i+spacingR1) * (Math.PI/180))),
-      x2After = Math.round(cx + r2 * Math.cos((i+spacingR2) * (Math.PI/180))),
-      y2After = Math.round(cy + r2 * Math.sin((i+spacingR2) * (Math.PI/180)));
+    if (custom) {
+      steps = buildStepsArr(children, custom);
+    }
 
-      // points[flag] = {
-      //   point1: {
-      //     x: x1,
-      //     y: y1
-      //   },
-      //   point2: {
-      //     x: x2,
-      //     y: y2
-      //   }
-      // };
+    for (var i=0, j=0; i<=360; i=custom?i+steps[j]:i+Number(step.toFixed(1)), j++) {
+      x1Before = Math.round(cx + r1 * Math.cos(Snap.rad(i-spacingR1Left))),
+      y1Before = Math.round(cy + r1 * Math.sin(Snap.rad(i-spacingR1Left))),
+      x2Before = Math.round(cx + r2 * Math.cos(Snap.rad(i-spacingR2Left))),
+      y2Before = Math.round(cy + r2 * Math.sin(Snap.rad(i-spacingR2Left))),
+      x1After = Math.round(cx + r1 * Math.cos(Snap.rad(i+spacingR1Right))),
+      y1After = Math.round(cy + r1 * Math.sin(Snap.rad(i+spacingR1Right))),
+      x2After = Math.round(cx + r2 * Math.cos(Snap.rad(i+spacingR2Right))),
+      y2After = Math.round(cy + r2 * Math.sin(Snap.rad(i+spacingR2Right)));
 
       points[flag] = {
         before: {
@@ -78,35 +126,49 @@
     return points;
   };
 
+  /** get index of the current object from arrays */
+  function getIndex(search_item, array) {
+    for (var i=0; i<array.length; i++) {
+      if (array[i] === search_item) {
+        return i;
+      }
+    }
+  }
+
   /** radialMenu constructor */
   var radialMenu = function (options) {
     this.defaults = {
       "stroke": 1, //stroke width around every menu item, in pixels
-      "spacing": 20, //amount of space between menu items
-      "x": 100, //x location of the center of the menu
-      "y": 100, //y location of the center of the menu
-      "opacity": 1.0, //opacity of the main menu
+      "spacing": 10, //amount of space between menu items
+      "x": 0, //x location of the center of the menu
+      "y": 0, //y location of the center of the menu
+      "opacity": 1, //opacity of the main menu
       "size": null, //if set to a float value 0-1, attempt to allow the section to take that percent of the circle. if null, size is automatically calculated by
       "font-size": 14, //font size of this item, in pixels
       "font-family": 'Verdana',
       "font-color": '#000000',
-      "active-font-color": '#FF0000',
-      "active-stroke-color": '#FF0000',
+      "active-font-color": '#000000',
+      "active-stroke-color": '#000000',
       "active-stroke-opacity": 1.0,
-      "active-fill": '#FF0',
+      "active-fill": '#FFFFFF',
       "active-fill-opacity": 1.0,
-      "stroke-color": '#CCCCCC',
+      "stroke-color": '#000000',
       "stroke-opacity": 1.0,
-      "fill": '#DDD',
+      "fill": '#FFFFFF',
       "fill-opacity": 0.5,
       "data": null, //optionally additional object can be provided that will be sent to the callback, this could include an ID or URL to load on click
       "onclick": null, //callback, none by default
-      "circlesSpacing": 2
+      "circles-spacing": 0,
+      "start-radius": 50
     };
     this.options = extend(this.defaults, options);
     this.parentOptions = this.options;
 
-    this.init();
+    if (self.Snap) {
+      this.init();
+    } else {
+      console.log("Snap.svg library is required");
+    }
   };
 
   radialMenu.prototype = {
@@ -117,12 +179,18 @@
       this.body = document.querySelector("body");
       this.xmlns = "http://www.w3.org/2000/svg";
       this.svg = document.createElementNS (this.xmlns, "svg");
+      // get svg from ID
+      // this.svg = document.getElementById("menu-svg");
 
-      // snap svg element
+      // radialMenu SVG elements
       this.s = Snap(this.svg);
       this.g;
-      this.radiusBig = 200;
-      this.radiusSmall = 60;
+      this.mainGroup = this.s.group();
+      this.mainGroup.addClass("radialMenu-holder");
+
+      // radiuses
+      this.radiusBig;
+      this.radiusSmall = this.options["start-radius"];
 
       // empty arrays too keep radial item objects
       this.childs = [];
@@ -131,8 +199,10 @@
       this.defs = [];
 
       // if opened
-      this.isOpened = true;
+      this.action_flag = false;
     },
+
+    /** finding new radiuses. Default - +50px */
     calculateRadiuses: function () {
       var text = "",
         textLength,
@@ -153,41 +223,74 @@
         textRadius = textLength.node.clientWidth / (2*Math.PI);
 
         this.radiusSmall = this.parent?this.parent.radiusBig + 10:this.radiusSmall;
-        this.radiusBig = textRadius+(textRadius-this.radiusSmall);
+        this.radiusBig = textRadius+(textRadius-this.radiusSmall)+10;
       } else {
         this.radiusSmall = this.parent?this.parent.radiusBig + 10:this.radiusSmall;
         this.radiusBig = this.radiusSmall + 50;
       }
     },
+
+    /** get menu holder */
+    get: function () {
+      return this.mainGroup;
+    },
+
+    /** open method for each object */
     open: function () {
-      if (this.isOpened) {
-        this.buildSvg();
-        this.buildChildren();
-        this.isOpened = false;
+      var self = this;
+
+      if (!this.action_flag) {
+        if (!this.parent) {
+          this.buildSvg();
+          this.buildChildren();
+        } else {
+          this.parent.childs.forEach(function (el) {
+            if (el.action_flag){
+              el.closeChildren();
+            }
+          });
+          if (!this.parent.action_flag) {    
+            this.parent.open();
+          }
+          
+          this.buildChildren();
+          this.parent.removeActive();
+          this.parent.addActive(getIndex(this, this.parent.childs));
+        }
       }
     },
+
+    /** creating SVG element and paste it to body */
     buildSvg: function () {
       this.svg.setAttributeNS (null, "width", "100%");
       this.svg.setAttributeNS (null, "height", "100%");
-      this.svg.setAttribute("style", "position: fixed; left: 0; top: 0;");
-      // this.svg.setAttribute("viewbox", "0 0 800 600");
+      this.svg.setAttribute("style", "position: absolute; left: 0; top: 0;");
 
+      this.mainGroup.attr("opacity", this.options.opacity);
+      // this.mainGroup.transform("0");
       this.body.insertBefore(this.svg, this.body.firstChild);
     },
+
+    /** all drawing magic is here */
     buildChildren: function () {
       var step = 360/this.childs.length,
         points,
         self = this,
         childs_length,
-        matrix = new Snap.Matrix();
+        matrix = new Snap.Matrix(),
+        x_center = this.options.x,
+        y_center = this.options.y,
+        custom = checkSize(this.childs);
 
+      // add flag means menu is opened
+      this.action_flag = true;
       // Calculating radiuses before children builds
       this.calculateRadiuses();
-      points = circlePoints(step, this.radiusBig, this.radiusSmall, this.options.x, this.options.y, this.options.spacing);
+      // building points for drawing
+      points = circlePoints(this.childs, step, this.radiusBig, this.radiusSmall, x_center, y_center, this.options.spacing);
       childs_length = Object.keys(points).length;
-
       matrix.scale(1.2, 1.2);
-      this.g = this.s.group();
+      this.g = this.mainGroup.group();
 
       for (var i=0; i<childs_length-1; i++) {
         (function (i) {
@@ -196,76 +299,103 @@
             point1Next = points[i+1].before.point1.x + " " + points[i+1].before.point1.y,
             point2Next = points[i+1].before.point2.x + " " + points[i+1].before.point2.y,
             middlePoint1 = (points[i].after.point1.x+points[i].after.point2.x)/2 + " " + (points[i].after.point1.y+points[i].after.point2.y)/2,
-            middlePoint2 = (points[i+1].before.point1.x+points[i+1].before.point2.x)/2 + " " + (points[i+1].before.point1.y+points[i+1].before.point2.y)/2;
+            middlePoint2 = (points[i+1].before.point1.x+points[i+1].before.point2.x)/2 + " " + (points[i+1].before.point1.y+points[i+1].before.point2.y)/2,
+            this_size = self.childs[i].options.size;
 
           self.circles.push(self.g.path("M " + point1 + 
-            " A " + self.radiusBig + " " + self.radiusBig + ", 0, 0, 1 " + point1Next + 
+            " A " + self.radiusBig + " " + self.radiusBig + (custom?((this_size<=0.5)?" 0, 0, 1 ":" 0, 1, 1 "):" 0, 0, 1 ") + point1Next + 
             " L " + point2Next + 
-            " A " + self.radiusSmall + " " + self.radiusSmall + ", 0, 0, 0 " + point2 + " Z")
+            " A " + self.radiusSmall + " " + self.radiusSmall + (custom?((this_size<=0.5)?" 0, 0, 0 ":" 0, 1, 0 "):" 0, 0, 0 ") + point2 + " Z")
           .attr({
             "strokeWidth": self.options["stroke"],
             "stroke": self.childs[i].options["stroke-color"],
             "stroke-opacity": self.childs[i].options["stroke-opacity"],
             "fill": self.childs[i].options["fill"],
-            "fill-opacity": self.childs[i].options["fill-opacity"]
+            "fill-opacity": self.childs[i].options["fill-opacity"],
+            "cursor": "pointer"
           })
           .click(function () {
+            if (self.childs[i].options.onclick) {
+              self.childs[i].options.onclick();
+            }
             if (!self.childs[i].action_flag) {
-              self.childs.forEach(function (el) {
-                el.closeChildren();
-              });
-              self.removeActive();
-              self.addActive(i);
-              if (self.childs[i].options.onclick) {
-                self.childs[i].options.onclick();
-              } else {
-                setTimeout(function () {
-                  self.childs[i].buildChildren();
-                  self.childs[i].action_flag = true;
-                }, 100);
-              }
+              self.childs[i].open(i);
             }
           }));
           
           if (points[i].after.point1.x <= points[i+1].before.point1.x) {
             self.texts.push(self.g.text(0, 0, self.childs[i].label).attr({
-              "textpath": "M " + middlePoint1 + " A " + (self.radiusBig+self.radiusSmall)/2 + " " + (self.radiusBig+self.radiusSmall)/2 + ", 0, 0, 1 " + middlePoint2
+              "textpath": "M " + middlePoint1 + " A " + (self.radiusBig+self.radiusSmall)/2 + " " + (self.radiusBig+self.radiusSmall)/2 + (custom?((this_size<=0.5)?" 0, 0, 1 ":" 0, 1, 1 "):" 0, 0, 1 ") + middlePoint2
             }));
           } else {
             self.texts.push(self.g.text(0, 0, self.childs[i].label).attr({
-              "textpath": "M " + middlePoint2 + " A " + (self.radiusBig+self.radiusSmall)/2 + " " + (self.radiusBig+self.radiusSmall)/2 + ", 0, 0, 0 " + middlePoint1
+              "textpath": "M " + middlePoint2 + " A " + (self.radiusBig+self.radiusSmall)/2 + " " + (self.radiusBig+self.radiusSmall)/2 + (custom?((this_size<=0.5)?" 0, 0, 0 ":" 0, 1, 0 "):" 0, 0, 0 ") + middlePoint1
             }));
           }
           self.texts[i].attr({
-            "fill": self.options["font-color"],
-            "font-family": self.options["font-family"],
-            "font-size": self.options["font-size"],
+            "fill": self.childs[i].options["font-color"],
+            "font-family": self.childs[i].options["font-family"],
+            "font-size": self.childs[i].options["font-size"],
             "text-anchor": "middle",
-            "pointer-events": "none"
+            "pointer-events": "none",
+            "alignment-baseline": "baseline"
           });
-          self.texts[i].textPath.attr("startOffset", "50%");
+          self.texts[i].textPath.attr({
+            "startOffset": "50%",
+            "alignment-baseline": "middle"
+          });
         })(i);
       }
       this.addAnimationIn();
     },
+
+    /** close current item */
     closeChildren: function () {
       if (this.g) {
         this.addAnimationOut();
       }
     },
+
+    /** close childs of every item */
     closeAllChildren: function () {
       this.childs.forEach(function (el) {
         el.closeChildren();
       });
     },
+
+    /** closing each circle */
     close: function () {
+      var self = this,
+        length;
+
+      if (!this.parent) {
+        length = this.mainGroup.selectAll("g").length;
+        setTimeout(function () {
+          self.clearWholeMenu();
+        }, length*60);
+      }
+
+      if (this.g && !this.g.removed) {
+        this.g.remove();
+        this.circles = [];
+        this.texts = [];
+        this.defs = [];
+        this.action_flag = false;
+        this.closeAllChildren();
+      }
+    },
+
+    /** remove all snapSVG elements after all circles was removed */
+    clearWholeMenu: function () {
       this.g.remove();
       this.circles = [];
       this.texts = [];
       this.defs = [];
       this.action_flag = false;
-      this.closeAllChildren();
+      this.s.remove();
     },
+
+    /** remove active  */
     removeActive: function () {
       var self = this;
 
@@ -280,27 +410,26 @@
 
       this.texts.forEach(function (el, index) {
         el.attr({
-          "fill": self.options["font-color"]   
+          "fill": self.childs[index].options["font-color"]   
         });
       });
-
-      this.childs.forEach(function (el, index) {
-        el.action_flag = false;
-      });
     },
-    addActive: function (index) {
 
+    /** add active styles */
+    addActive: function (index) {
       this.circles[index].attr({
-        "stroke": this.options["active-stroke-color"],
-        "stroke-opacity": this.options["active-stroke-opacity"],
-        "fill": this.options["active-fill"],
-        "fill-opacity": this.options["active-fill-opacity"]  
+        "stroke": this.childs[index].options["active-stroke-color"],
+        "stroke-opacity": this.childs[index].options["active-stroke-opacity"],
+        "fill": this.childs[index].options["active-fill"],
+        "fill-opacity": this.childs[index].options["active-fill-opacity"]  
       });
 
       this.texts[index].attr({
-        "fill": this.options["active-font-color"]
+        "fill": this.childs[index].options["active-font-color"]
       });
     },
+
+    /** animating each circle in */
     addAnimationIn: function () {
       var group = this.g;
 
@@ -311,8 +440,10 @@
       this.g.animate({ 
         transform: "r0," + this.g.getBBox().x + ',' + this.g.getBBox().y + "s1,1," + this.g.getBBox().cx + "," + this.g.getBBox().cy,
         opacity: 1
-      } ,300, mina.easeout);
+      } , 300, mina.easeout);
     },
+
+    /** animating each circle out. Callback - close next level circle */
     addAnimationOut: function () {
       var self = this;
 
@@ -341,7 +472,6 @@
       childrenRecursion(this.childs);
       return children_arr;
     },
-    /** end return children */
 
     /** updating options */
     update: function (new_options) {
@@ -358,8 +488,8 @@
       }
       updateOptions(this.childs);
     },
-    /** end updating options */
 
+    /** method to add children */
     add: function (label, options) {
       return new myMenuItem(label, options, this, this.parentOptions);
     }
@@ -379,20 +509,20 @@
     this.texts = [];
     this.defs = [];
 
+    // push just created menu item to parents items array
     this.parent.childs.push(this);
+
+    // menuItem SVG elements
     this.svg = this.parent.svg;
     this.s = this.parent.s;
+    this.mainGroup = this.parent.mainGroup;
     this.g;
 
     // action flag
     this.action_flag = false;
-
-    // radiuses
-    // this.radiusSmall = this.parent.radiusBig + 10;
-    // this.radiusBig = this.radiusSmall + 50;
   };
 
-  myMenuItem.prototype = new radialMenu();
+  myMenuItem.prototype = radialMenu.prototype;
 
   window.radialMenu = radialMenu;
 })(window);
